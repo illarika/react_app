5515c7fd425f773215374bb352e171df7e4ee954
Olga Liahor <liahor.olga@gmail.com>
2022-02-14 14:42:23 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import MyButton from "./components/MyButton/MyButton";
-import { useState } from "react";
-import ToggleButton from "./components/MyButton/ToggleButton";
-
+import UserAdd from "./components/UserAdd";
+import UserList from "./components/UserList";
+import { users } from "./users";
 const App= () =>{
-  const [buttonText, setButtonText] = useState('Click me please!');
-  const [toggle, setToggle] = useState(false);
+  // const [buttonText, setButtonText] = useState('Click me please!');
+  // const changeText = (text) =>{
+  //   setButtonText("New Text")
+  // }
+  // const [toggle, setToggle] = useState(false);
+  
+  console.log(users)
+  
   return (
     <div className="App">
-      <MyButton changeText={setButtonText}>{buttonText}</MyButton>
+      <UserList users= {users}/>
+      <UserAdd/>
+
+
+
+      {/* <MyButton changeText={setButtonText}>{buttonText}</MyButton>
+
+
     <ToggleButton toggle={toggle} setToggle={setToggle}>{toggle ? 'One' : 'Two'}</ToggleButton>
 
+    <Counter></Counter>
+     <MyClassButton></MyClassButton> */}
+
+     
+
+      
 
      {/* <MyButton description = "some button">Text button for visible and enjoy</MyButton> */}
     </div>
