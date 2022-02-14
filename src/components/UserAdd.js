80f3774f37c5cacff4a579fa93f48c07da815af5
Olga Liahor <liahor.olga@gmail.com>
2022-02-14 16:07:36 +0300

    first

---
+++ src/components/UserAdd.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {useState} from "react";
+
+const UserAdd = () => {
+    const [user, setUser] = useState({});
+    const onChange = (e) => {
+        const field = e.target.id;
+        setUser( {...user,[field]: e.target.value})
+    };
+    console.log(user);
+   
+    const values = {
+        name: 'text',
+        age: 'number',
+        country: 'text',
+    };
+
+    return (
+        <>
+            {Object.keys(values).map((value, index) =>
+                <input
+                    id={value}
+                    key={index}
+                    type={values[value]}
+                    placeholder={`Input user ${value}`}
+                    onChange={onChange}
+                />
+                )}
+            <button>Add new User</button>
+            
+      
+        </>
+    );
+};
+
+export default UserAdd;
